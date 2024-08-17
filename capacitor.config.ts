edd6d9f0a8907291233294b973796e879bf4cfe2
Eduardo Reyes <ed.reyes@duocuc.cl>
2024-08-16 20:50:25 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PrimeraAplicacion',
+  webDir: 'www'
+};
+
+export default config;
